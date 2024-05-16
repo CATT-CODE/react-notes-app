babdce0f0e816341a7f4ebaafa27b5997b96502d
CATT-CODE <65243972+CATT-CODE@users.noreply.github.com>
2024-05-16 14:14:16 -0400

    General style changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,6 +2,7 @@ import { useContext } from 'react';
 import { Navbar, Nav, Button, Container } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
 import { NoteContext } from '../context/NoteContext';
+import './Header.css'; // Import the CSS file for additional styles
 
 function Header() {
   const { darkMode, toggleDarkMode } = useContext(NoteContext);
@@ -11,7 +12,7 @@ function Header() {
       <Container>
         <Navbar.Brand href="/">Notes App</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
+        <Navbar.Collapse id="basic-navbar-nav" className="d-lg-flex justify-content-lg-between align-items-lg-center">
           <Nav className="mr-auto">
             <LinkContainer to="/">
               <Nav.Link>Home</Nav.Link>
@@ -20,13 +21,19 @@ function Header() {
               <Nav.Link>Add Note</Nav.Link>
             </LinkContainer>
           </Nav>
-          <Button variant="secondary" onClick={toggleDarkMode}>
-            {darkMode ? <img src='/sun.png' /> : <img src='/moon.png' />}
-          </Button>
+          <Nav className="ml-lg-auto">
+            <Button 
+              variant="secondary" 
+              className={`dark-mode-toggle`} 
+              onClick={toggleDarkMode}
+            >
+              {darkMode ? <img src='/sun.png' alt="Light Mode" /> : <img src='/moon.png' alt="Dark Mode" />}
+            </Button>
+          </Nav>
         </Navbar.Collapse>
       </Container>
     </Navbar>
-  )
+  );
 }
 
-export default Header
+export default Header;
