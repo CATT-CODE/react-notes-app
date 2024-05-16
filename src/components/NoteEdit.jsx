013749177da9ca24bd283549c9fe810638ff058e
CATT-CODE <65243972+CATT-CODE@users.noreply.github.com>
2024-05-16 11:57:25 -0400

    Basic view/edit a single note page

--- src/components/NoteEdit.jsx
+++ src/components/NoteEdit.jsx
@@ -1,8 +1,51 @@
+import { useContext } from 'react';
+import { useParams, useNavigate } from 'react-router-dom';
+import { NoteContext } from '../context/NoteContext';
+import { Form, Button } from 'react-bootstrap';
 
 function NoteEdit() {
+  const { id } = useParams();
+  const { notes, deleteNote, updateNote } = useContext(NoteContext);
+  const navigate = useNavigate();
+
+  const note = notes.find(note => note.id === Number(id));
+
+  const handleUpdate = (e) => {
+    e.preventDefault();
+    const updatedText = e.target.elements.noteText.value;
+    updateNote({
+      ...note,
+      text: updatedText
+    });
+    navigate('/');
+  };
+
   return (
-    <div>NoteEdit</div>
-  )
+    <>
+      <Form onSubmit={handleUpdate}>
+        <Form.Group>
+          <Form.Label>Note Text</Form.Label>
+          <Form.Control
+            as="textarea"
+            rows={3}
+            defaultValue={note.text}
+            name="noteText"
+          />
+        </Form.Group>
+        <Button variant="primary" type="submit" className="mt-3 mr-2">Save</Button>
+        <Button
+          variant="danger"
+          className="mt-3 ml-2"
+          onClick={() => {
+            deleteNote(note.id);
+            navigate('/');
+          }}
+        >
+          Delete
+        </Button>
+      </Form>
+    </>
+  );
 }
 
 export default NoteEdit
